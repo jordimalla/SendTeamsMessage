2a15c226aa071119fc36786a2ed812053175b8cb
Jordi Malla <48735113+jordimalla@users.noreply.github.com>
2021-04-23 10:34:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup
+from SendTeamsMessage import __version__
 
+setup(
+    name='SendTeamsMessage',
+    version=__version__,
+    description='A package to send a little message to your Teams channel',
+    url='git@github.com:jordimalla/SendTeamsMessage.git',
+    author='Jordi Malla',
+    license='MIT',
+    packages=['SendTeamsMessage'],
+    zip_safe=True
+)
